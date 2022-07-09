9b8f47789ff19cdee6840cbf9b3e551e032dfdc3
bentoag <profbentoag@gmail.com>
2022-07-09 08:55:19 -0300

    update exerc6

--- week1/src/week1/Exerc6.java
+++ week1/src/week1/Exerc6.java
@@ -9,7 +9,18 @@ import java.util.Scanner;
 public class Exerc6 {
 
 	public static void main(String[] args) {
+		int anonasc=0, anoatual=0, x=0, y=0;
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("Informe o ano que você nasceu: ");
+		anonasc = sc.nextInt();
+		System.out.print("Informe o ano atual: ");
+		anoatual = sc.nextInt();
+		
+		x=anoatual - anonasc;
+		y=x-1;
+		
+		System.out.println("Sua idade é " + x + " ou " + y);
 	}
 
 }
