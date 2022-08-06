31e3abb20b33876a388664e5b737ef6a84d9b714
bentoag <profbentoag@gmail.com>
2022-08-06 08:47:26 -0300

    recursividade
    aplicacao recursividade para retornar string invertida

--- modulo2/src/week1/Pessoa.java
+++ modulo2/src/week1/Pessoa.java
@@ -1,14 +1,31 @@
 package week1;
 
 public class Pessoa {
-
+	
+	//com recursividade
 	String retornaTexto(String texto) {
 
-		String reverso = "";
-
-		for (int i = (texto.length() - 1); i >= 0; i--) {
-			reverso += texto.charAt(i);
-		}
-		return reverso;
+		String reverso = "", stringTemp = "";
+		int tamanhoString = texto.length()-1;
+		
+			while (tamanhoString > 0) {
+				reverso+=texto.charAt(tamanhoString); // ultimo caracter
+				tamanhoString--;
+				stringTemp = texto.substring(0, tamanhoString);
+				retornaTexto(stringTemp);
+			}
+			
+			if(tamanhoString==0) {
+				reverso += texto.charAt(0);
+			}
+			return reverso;
 	}
 }
+	
+	
+//metodo tradicional utilizar um for para percorrer a string e concatenar em outra variavel
+//		for (int i = (texto.length() - 1); i >= 0; i--) {
+//			reverso += texto.charAt(i);
+//		}
+	
+
--- modulo2/src/week1/PessoaMain.java
+++ modulo2/src/week1/PessoaMain.java
@@ -5,8 +5,7 @@ public class PessoaMain {
 	public static void main(String[] args) {
 		Pessoa francisco = new Pessoa();
 		Pessoa bento = new Pessoa();
-		
-		
+		Pessoa maria = new Pessoa();
 		
 		System.out.println("--- Francisco ---");
 		invertido = francisco.retornaTexto("Francisco");
@@ -14,7 +13,9 @@ public class PessoaMain {
 		System.out.println("--- Bento ---");
 		invertido = bento.retornaTexto("Bento");
 		System.out.println(invertido);
-		
+		System.out.println("--- Maria ---");
+		invertido = maria.retornaTexto("Maria Josefa Silveira");
+		System.out.println(invertido);
 		
 	}
 
